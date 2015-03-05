8c164517e9adb8db9e1bea9fc38d102d3a1dae85
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-05 15:44:03 -0500

    day2 var
    day 2 var

--- day2/js/script.js
+++ day2/js/script.js
@@ -17,3 +17,29 @@ Anything inside of here will not be seen by computer
 //alert box- pop up box that will alert the user with information.
 
 alert("text that the user will see");
+//great for checking values and debugging
+//also shows all errors
+console.log("this is the console.")
+
+//declare a variable
+//use the keyword of VAR
+var whatever1;
+
+//define the variable
+//set tthe value of the existing variable
+whatever1 = 42;
+
+
+//concole.log the variable
+console.log ("the variable whatever1 is")
+console. log (whatever1)
+
+//declare and define a variable at the dame time
+var a = 2;
+
+console.log (a)
+
+a+3;
+a=a+3;
+console. log(a);
+
