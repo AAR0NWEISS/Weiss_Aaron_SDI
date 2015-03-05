4f941f84bddf0ca9406aa2a09b2b447ae69a945e
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-05 14:55:58 -0500

    day2
    first day coding

---
+++ day2/js/script.js
@@ -0,0 +1,19 @@
+/*
+Aaron Weiss
+5MAR2015
+SEC00
+Day 2 Into
+ */
+
+//alert("testing to see if conn");
+
+/*
+example of a multilined comment
+Anything inside of here will not be seen by computer
+ */
+
+// single line comment. only this line will be ignored.
+
+//alert box- pop up box that will alert the user with information.
+
+alert("text that the user will see");
