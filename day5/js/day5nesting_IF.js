2bfc3e65a038c35c9595893f12f5b23b44c445b6
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-12 15:03:13 -0400

    learned about nesting
    learned about nest

--- day5/js/day5nesting_IF.js
+++ day5/js/day5nesting_IF.js
@@ -5,3 +5,32 @@
  Day 2 Into
  */
 
+//alert("test")
+
+// some decisions affect other decisions
+
+//if it is warm then lets go to the beach, if it is not ten go to the movies
+
+
+//get temparature
+var temp=86;
+var waterTemp=78;
+
+if(temp <=80){
+    console.log("go to the beach");
+
+}else(temp >=80){
+    console.log("go to the movies");
+}
+if(waterTemp >75){
+    console.log("lets go swiming");
+}else{
+    console.log("go simming")
+}
+
+
+//do you have kids in group
+var kids = "no";
+//test if weh have kids
+if(kids==="no");
+    console.log ("let's see 50 shaeds of gray")
