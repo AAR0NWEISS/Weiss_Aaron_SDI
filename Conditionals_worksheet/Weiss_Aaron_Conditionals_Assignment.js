0c825370de9e1c0f414dbe93c4078ca4448825ab
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-14 15:01:51 -0400

    math day6
    math day 6

--- Conditionals_worksheet/Weiss_Aaron_Conditionals_Assignment.js
+++ Conditionals_worksheet/Weiss_Aaron_Conditionals_Assignment.js
@@ -1,11 +1,5 @@
 /**
  * Created by aweiss on 3/12/15.
- Aaron Weiss
- SEC00
- Conditionals_Assignment.
-
-
-
  */
 
 
---
+++ day6/day6math.js
@@ -0,0 +1,9 @@
+/*
+ Aaron Weiss
+ 5MAR2015
+ SEC00
+ Day 6 math
+ */
+
+
+alert("test")
