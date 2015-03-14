8b91f2319f77f50544bc30f3eeedcac7b03badb0
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-14 16:59:07 -0400

    end

--- day6/day6forloops.js
+++ day6/day6forloops.js
@@ -1,3 +1,56 @@
 /**
  * Created by aweiss on 3/14/15.
  */
+//aaron weiss
+//sec00
+
+
+
+
+//for(intializaatoin: condition increment of change) {}
+//we use i ALOT in loops becasue if stands for iteratoin
+//tiem around
+
+
+
+for(var i=0; i<20; i++){
+    console.log("the value of i is "+i)
+
+}
+
+console.log(i);
+
+
+
+///break in a loop
+
+for(var j=0; j<5; j++){
+//conditoin to test if we should break the loop
+    //if the value of J is 3, that conslol.log somthing different and stop the loop
+    if(j===3){
+        //stops the loop but first console.log out
+        console.log("'3 is 3!!!")
+        break;
+    }
+    console.log("the value of J is "+j)
+
+}
+
+
+//var (var j=5; j>0; j--)
+
+
+
+
+//sepecific case for each value
+
+for(var k=0; k<3; k++){
+    if(k===0){
+        console.log("the value is zero")
+    }else if(k===1){
+        consloe.log("the value is zero")
+    }else if (k===2){
+
+    }
+
+}
