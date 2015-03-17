8363240da8d4eb8757e4a9e387a8fdbf47ceb837
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-17 16:30:59 -0400

    arg
    arg

--- day7/day7arg.js
+++ day7/day7arg.js
@@ -3,4 +3,53 @@
  */
 
 
-alert("test")
+//alert("test")
+
+
+//arguments -- goes into the functoin part of the functoin call
+//[arametars -- catch the argument and in the functoin defintion
+
+
+//creat a functoin that calculateds the are of a rectangle.... again!!!!
+
+//add in parameter
+function calcArea(w,h){
+
+    //var  width=10;
+    //var hieght=20;
+    var area=w+h;
+    console.log("the area is "+area);
+
+}
+
+
+//call our functoin
+//add arguments to the funcotin call
+
+calcArea(10,20);
+calcArea(40,60);
+
+
+//prompt user for wdith and height
+
+var width=prompt("enter a width");
+
+//enter validatoin
+var height=prompt("enter a height");
+
+calcArea(width,height);
+
+//dog years
+function dogYear(humanAge){
+    //dog years = human years +7
+    var dogAge=humanAge*7;
+    console.log("Your age in dog years is "+dogAge)
+}
+
+//call dog functoin
+dogYear(4);
+
+var userInput = prompt ("Please enter your human age");
+
+//call the dog functoin
+dogYear(userInput);
