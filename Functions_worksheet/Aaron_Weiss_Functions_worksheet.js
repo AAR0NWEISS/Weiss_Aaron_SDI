f1c2fe6b79edfd3965efa1c0055232bb36842d11
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-17 18:52:32 -0400

    add var to main code
    ad var to main code

--- Functions_worksheet/Aaron_Weiss_Functions_worksheet.js
+++ Functions_worksheet/Aaron_Weiss_Functions_worksheet.js
@@ -4,6 +4,8 @@
 
 
 //alert("test")
+/////////////////////////////////////////////////////Circumference///////////////////////////////////////////////////////////////////
+var radius = 2;
 
 function calculatorCircumference(r){//function = create factory and call it "calculatorCircumference" (R)=floating variable
 
@@ -15,7 +17,19 @@ function calculatorCircumference(r){//function = create factory and call it "cal
 
 //calculatorCircumference(4);//calls output from function, i.g. runs code for calculatorCirumference
 //or
-var circumferenceMAINcode = calculatorCircumference(4);//stores output from function into var circumferenceMAINcode
+var circumferenceMAINcode = calculatorCircumference(radius);//stores output from function into var circumferenceMAINcode
 console.log("The circumference of the circle is "+circumferenceMAINcode+".");//Output from var circumferenceMAINcode in main code
 
 
+//////////////////////////////////////////////////////////Stung!//////////////////////////////////////////////////////////////////////////
+var numOfPounds=5;
+
+function calculatorBee(w){
+    var wightINpounds=8.666666667*w;
+    console.log(wightINpounds);
+    return wightINpounds;
+
+}
+
+var wINpound=calculatorBee(numOfPounds);
+console.log("It takes "+wINpound+" bee stings to kill this animal.")
