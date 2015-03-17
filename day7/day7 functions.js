4f1b8ecfc3315981f803bbffa0f7c7eaf34dc525
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-17 15:15:11 -0400

    broken
    broken

--- day7/day7 functions.js
+++ day7/day7 functions.js
@@ -1,4 +1,19 @@
 /**
  * Created by aweiss on 3/17/15.
  */
-alert ('test')
+//alert ('test')
+
+
+//basic structure of a funtoin
+//functoin functoinName() { log out hello}
+
+//create a functoin that will console log out hello
+function printHello(){
+
+    console.log("Hello and Howdy")
+}
+
+//functoin call the printHello functoin
+// f
+
+print();
