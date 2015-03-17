9d43bf6db6ec165265bc13a1c4d0fea04f6cf13b
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-17 15:25:43 -0400

    functoin
    funcoitn

--- day7/day7 functions.js
+++ day7/day7 functions.js
@@ -13,7 +13,32 @@ function printHello(){
     console.log("Hello and Howdy")
 }
 
+function printMore(){
+
+    console.log ('print more text')
+}
+
 //functoin call the printHello functoin
 // f
 
-print();
+printHello();
+printMore();
+printHello();
+
+
+///creat a functoin that calculates the are of a retangle
+
+function calcArea(){
+
+    // creat var for width, heigh and area
+    var width = 20
+    var hight = 30
+    var area = width+hight
+
+    //prin out the area
+    console.log("The are is "+area)
+}
+
+//functoin call
+calcArea();
+
