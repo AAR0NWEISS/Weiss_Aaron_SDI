72a8b589ad2ac32a061eeac84c6fe31ce231d171
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-17 18:25:50 -0400

    save1
    save1

--- Functions_worksheet/Aaron_Weiss_Functions_worksheet.js
+++ Functions_worksheet/Aaron_Weiss_Functions_worksheet.js
@@ -3,4 +3,17 @@
 //Functions_worksheet
 
 
-//
+//alert("test")
+
+function calculatorCircumference(r){//function = create factory and call it "calculatorCircumference" (R)=floating variable
+
+    var circumference=r*2*Math.PI;//create var called circumference,
+    console.log(circumference);//output to console ECHO
+    return circumference;//relocates function var to main code
+
+}
+
+calculatorCircumference(4);
+//or
+var circumferenceMAINcode = calculatorCircumference(4);
+console.log("The Circumference of the Circle is "+circumferenceMAINcode+".");
