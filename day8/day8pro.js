08f0271de949d3e1f1e6106894d25cc4a4af917f
AAR0NWEISS <aoweiss@fullsail.edu>
2015-03-24 15:09:29 -0400

    end pro
    end pro

--- day8/day8pro.js
+++ day8/day8pro.js
@@ -2,6 +2,31 @@
  * Created by aweiss on 3/24/15.
  */
 
-alert('test');
+//alert('test');
 
-console.log("hello");
+//console.log("hello");
+
+
+
+function calArea(w,h){
+    var area = w*h;
+    return area;
+
+}
+
+//functoin call
+var area = calArea(6,8);
+console.log("The area of the rect is "+area);
+
+// this is a procdure
+//a list of steps to do
+function calAreaP(w,h){
+    var area = w+h;
+
+    //we do Not return this value
+    console.log("Inside the procedure the area is "+area)
+}
+
+//functoin cal for the procedure\
+//just the functoin name no variable to catch it
+calAreaP(12,15);
